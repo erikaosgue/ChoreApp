72130318edc303e32138a40b6c7b2e43d0ac3ad1
erikaosgue <1476@holbertonschool.com>
2021-03-25 20:17:06 -0500

    Create the ChoresDatabase class

--- app/src/main/java/com/erikaosgue/choresapp/activity/MainActivity.kt
+++ app/src/main/java/com/erikaosgue/choresapp/activity/MainActivity.kt
@@ -3,13 +3,14 @@ package com.erikaosgue.choresapp.activity
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.erikaosgue.choresapp.data.ChoresDatabaseHandler
 import com.erikaosgue.choresapp.databinding.ActivityMainBinding
 import com.erikaosgue.choresapp.model.Chore
 
 class MainActivity : AppCompatActivity() {
 
-	var dbHandler: ChoresDatabaseHandler?= null
+	// Create an var Of our type class ChoresDatabaseHandler
 
 	lateinit var  activityMainBinding: ActivityMainBinding
  	override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,15 +18,23 @@ class MainActivity : AppCompatActivity() {
  		activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
  		setContentView(activityMainBinding.root)
 
-		dbHandler = ChoresDatabaseHandler(this)
+		// Create an instance of our Class ChoresDatabaseHandler()
+		val dbHandler = ChoresDatabaseHandler(this)
 
+		// Create an Instance of type Chore()
 		val chore = Chore()
-		chore.choreName
-		chore.assignedTo
-		chore.assignedBy
-		chore.timeAssigned
+		chore.choreName = "Clean Room"
+		chore.assignedTo = "James"
+		chore.assignedBy = "Carlos"
 
-		dbHandler?.createChore(chore)
+		dbHandler.createChore(chore)
+
+
+		//Read from database
+		val list = dbHandler.readAChore(2)
+		if (list.isNotEmpty()) {
+			Log.d("Item", list.toString())
+			println("Chore Name: ${list[0].choreName}, AssignedBy: ${list[0].assignedBy}, AssignedTo: ${list[0].assignedTo}, Time: ${list[0].timeAssigned}")
+		}
     }
-
 }
--- app/src/main/java/com/erikaosgue/choresapp/data/ChoresDatabaseHandler.kt
+++ app/src/main/java/com/erikaosgue/choresapp/data/ChoresDatabaseHandler.kt
@@ -6,41 +6,147 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteOpenHelper
 import android.database.sqlite.SQLiteDatabase
 import android.util.Log
+import android.widget.Toast
 import com.erikaosgue.choresapp.model.*
+import java.text.DateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ChoresDatabaseHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class ChoresDatabaseHandler(val context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
     override fun onCreate(db: SQLiteDatabase?) {
-        var CREATE_CHORE_TABLE = "CREATE TABLE " + TABLE_NAME + "(" + KEY_ID + " INTEGER_PRIMARY_KEY," +
-                KEY_CHORE_NAME + " TEXT," +
-                KEY_CHORE_ASSIGNED_BY + " TEXT," +
-                KEY_CHORE_ASSIGNED_TO + " TEXT," +
-                KEY_CHORE_ASSIGNED_TIME + " LONG" + ")"
+        val CREATE_CHORE_TABLE = "CREATE TABLE $TABLE_NAME (" +
+                "$KEY_ID INTEGER_PRIMARY_KEY, " +
+                "$KEY_CHORE_NAME (VARCHAR(256)), $KEY_CHORE_ASSIGNED_BY (VARCHAR(256)), " +
+                "$KEY_CHORE_ASSIGNED_TO (VARCHAR(256)), $KEY_CHORE_ASSIGNED_TIME INTEGER)"
         db?.execSQL(CREATE_CHORE_TABLE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db?.execSQL("DROP TABLE IF EXIST " + TABLE_NAME)
+        db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
 
+        //create a New table "Again"
         onCreate(db)
     }
 
-    //CRUD
+    /**
+     * CRUD - Create, Read, Update, Delete
+     **/
     fun createChore(chore: Chore) {
-        var db: SQLiteDatabase = writableDatabase
-        val values: ContentValues = ContentValues()
+
+        val db = this.writableDatabase
+
+        // values is an object that will store a set of values (hashMap)
+        val values = ContentValues()
+
+
+        //Putting a key-value into the Value Object
         values.put(KEY_CHORE_NAME, chore.choreName)
         values.put(KEY_CHORE_ASSIGNED_BY, chore.assignedBy)
         values.put(KEY_CHORE_ASSIGNED_TO, chore.assignedTo)
         values.put(KEY_CHORE_ASSIGNED_TIME, System.currentTimeMillis())
 
-        db.insert(TABLE_NAME, null, values)
+        // Inserting  to the db the Table name, column hack=null and the Values
+        val result = db.insert(TABLE_NAME, null, values)
+        if (result == (0).toLong()) {
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+            Log.d("DATA NOT INSERTED", "FAIL")
+        }
+        else {
+            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+            Log.d("DATA INSERTED", "SUCCESS")
+        }
 
-        Log.d("DATA INSERTED", "SUCCESS")
+//        Log.d("DATA INSERTED", "SUCCESS")
         db.close()
     }
-    fun readAChore(id: Int): Chore {
+
+
+    fun readAChore(id: Int): MutableList<Chore> {
+
+        val list: MutableList<Chore> = ArrayList()
+        val db: SQLiteDatabase = this.readableDatabase
+
+        val query = "Select * from $TABLE_NAME"
+        val result = db.rawQuery(query, null)
+        if (result.moveToFirst()) {
+            do {
+                val chore = Chore()
+                chore.choreName = result.getString(result.getColumnIndex(KEY_CHORE_NAME))
+                chore.assignedTo = result.getString(result.getColumnIndex(KEY_CHORE_ASSIGNED_TO))
+                chore.assignedBy = result.getString(result.getColumnIndex(KEY_CHORE_ASSIGNED_BY))
+                chore.timeAssigned = result.getLong(result.getColumnIndex(KEY_CHORE_ASSIGNED_TIME))
+                list.add(chore)
+                println("***** Chore Name: ${list[0].choreName},\n***** AssignedBy: ${list[0].assignedBy},\n***** AssignedTo: ${list[0].assignedTo}, Time: ${list[0].timeAssigned}")
+            }
+            while (result.moveToNext())
+        }
+        return list
+
+
+
+
+//        val db: SQLiteDatabase = readableDatabase
+
+//        val cursor: Cursor = db.query(
+//                TABLE_NAME,
+//                arrayOf(KEY_ID, KEY_CHORE_NAME, KEY_CHORE_ASSIGNED_BY,
+//                        KEY_CHORE_ASSIGNED_TO, KEY_CHORE_ASSIGNED_TIME),
+//                "$KEY_ID=?",
+//                arrayOf(id.toString()),
+//                null,
+//                null,
+//                null)
+////
+////        if (cursor != null)
+//        if(cursor.moveToFirst()) {
+//            val chore = Chore()
+//            chore.choreName = cursor.getString(cursor.getColumnIndex(KEY_CHORE_NAME))
+//            chore.assignedTo = cursor.getString(cursor.getColumnIndex(KEY_CHORE_ASSIGNED_TO))
+//            chore.assignedBy = cursor.getString(cursor.getColumnIndex(KEY_CHORE_ASSIGNED_BY))
+//            chore.timeAssigned = cursor.getLong(cursor.getColumnIndex(KEY_CHORE_ASSIGNED_TIME))
+////
+//            val dateFormat: DateFormat = DateFormat.getDateInstance()
+//            //Create a Date object
+//            val date = Date(cursor.getLong(cursor.getColumnIndex(KEY_CHORE_ASSIGNED_TIME))).time
+//            var formatDate = dateFormat.format(date)
+//            list.add(chore)
+//
+//        }
+//        else {
+//            println("Didn't enter")
+//        }
+//
+//        return list
+
+    }
+    fun updateChore(chore: Chore): Int {
         var db: SQLiteDatabase = writableDatabase
-        var cursor: Cursor = db.query(TABLE_NAME, arrayOf(KEY_ID))
+        var values = ContentValues()
+
+        //Putting a key-value into the Value Object
+        values.put(KEY_CHORE_NAME, chore.choreName)
+        values.put(KEY_CHORE_ASSIGNED_BY, chore.assignedBy)
+        values.put(KEY_CHORE_ASSIGNED_TO, chore.assignedTo)
+        values.put(KEY_CHORE_ASSIGNED_TIME, System.currentTimeMillis())
+
+        //Update a row
+
+        return db.update(TABLE_NAME, values, KEY_ID + "=?", arrayOf(chore.id.toString()))
+
+    }
+    fun deleteChore(chore: Chore) {
+
+        var db: SQLiteDatabase = writableDatabase
+        db.delete(TABLE_NAME, KEY_ID + "=?", arrayOf(chore.id.toString()))
+        db.close()
+    }
+    fun getChoreCount(): Int {
+        var db: SQLiteDatabase = readableDatabase
+        var countQuery = "SELECT * FROM $TABLE_NAME"
+        var cursor = db.rawQuery(countQuery, null)
+
+        return cursor.count
 
     }
 }
