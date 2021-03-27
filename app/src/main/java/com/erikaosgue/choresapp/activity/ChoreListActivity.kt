aac427086a0f2167447c9f23ed49f80e68cb34ed
erikaosgue <1476@holbertonschool.com>
2021-03-26 21:38:27 -0500

    Hot fix query creating the table

--- app/src/main/java/com/erikaosgue/choresapp/activity/ChoreListActivity.kt
+++ app/src/main/java/com/erikaosgue/choresapp/activity/ChoreListActivity.kt
@@ -1,11 +1,14 @@
-package com.erikaosgue.choresapp
+package com.erikaosgue.choresapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.erikaosgue.choresapp.R
 
 class ChoreListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chore_list)
+
+
     }
 }
