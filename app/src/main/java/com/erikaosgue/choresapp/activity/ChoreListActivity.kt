66c492c7c50ed1921585f8faae72dfb0ee496221
erikaosgue <1476@holbertonschool.com>
2021-03-26 21:37:54 -0500

    Update Chores Upp

---
+++ app/src/main/java/com/erikaosgue/choresapp/activity/ChoreListActivity.kt
@@ -0,0 +1,11 @@
+package com.erikaosgue.choresapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChoreListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chore_list)
+    }
+}
