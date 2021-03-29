2b77b7da34305d1856efe90502a39c41992325a8
erikaosgue <1476@holbertonschool.com>
2021-03-29 18:28:46 -0500

    Add delete and edit functions to chores app

--- app/src/main/java/com/erikaosgue/choresapp/data/ChoresDatabaseHandler.kt
+++ app/src/main/java/com/erikaosgue/choresapp/data/ChoresDatabaseHandler.kt
@@ -1,5 +1,6 @@
 package com.erikaosgue.choresapp.data
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
@@ -12,31 +13,38 @@ import java.text.DateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-class ChoresDatabaseHandler(val context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class ChoresDatabaseHandler(private val context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
     override fun onCreate(db: SQLiteDatabase?) {
+
+
         val CREATE_CHORE_TABLE = "CREATE TABLE $TABLE_NAME (" +
                 "$KEY_ID INTEGER PRIMARY KEY, " +
                 "$KEY_CHORE_NAME TEXT, $KEY_CHORE_ASSIGNED_BY TEXT, " +
                 "$KEY_CHORE_ASSIGNED_TO TEXT, $KEY_CHORE_ASSIGNED_TIME LONG)"
+
+
         Log.d("success", "Enter create: $CREATE_CHORE_TABLE")
+
         db?.execSQL(CREATE_CHORE_TABLE)
 
 
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+        // Drop "Delete the old Table"
         db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
 
         //create a New table "Again"
         onCreate(db)
     }
 
+
     /**
      * CRUD - Create, Read, Update, Delete
      **/
     fun createChore(chore: Chore) {
-
 
         val db: SQLiteDatabase = this.writableDatabase
 
@@ -52,16 +60,19 @@ class ChoresDatabaseHandler(val context: Context): SQLiteOpenHelper(context, DAT
 
         // Inserting  to the db the Table name, column hack=null and the Values
         val result = db.insert(TABLE_NAME, null, values)
+
         if (result == (0).toLong()) {
+
             Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
             Log.d("DATA NOT INSERTED", "FAIL")
         }
         else {
+
             Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
             Log.d("DATA INSERTED", "SUCCESS")
         }
 
-        Log.d("DATA INSERTED", "SUCCESS")
+
         db.close()
     }
 
@@ -77,12 +88,13 @@ class ChoresDatabaseHandler(val context: Context): SQLiteOpenHelper(context, DAT
         if (result.moveToFirst()) {
             do {
                 val chore = Chore()
+
+                chore.id = result.getInt(result.getColumnIndex(KEY_ID))
                 chore.choreName = result.getString(result.getColumnIndex(KEY_CHORE_NAME))
                 chore.assignedTo = result.getString(result.getColumnIndex(KEY_CHORE_ASSIGNED_TO))
                 chore.assignedBy = result.getString(result.getColumnIndex(KEY_CHORE_ASSIGNED_BY))
                 chore.timeAssigned = result.getLong(result.getColumnIndex(KEY_CHORE_ASSIGNED_TIME))
                 list.add(chore)
-                println("***** Chore Name: ${list[0].choreName},\n***** AssignedBy: ${list[0].assignedBy},\n***** AssignedTo: ${list[0].assignedTo}, Time: ${list[0].timeAssigned}")
             } while (result.moveToNext())
         }
         return list
@@ -91,8 +103,8 @@ class ChoresDatabaseHandler(val context: Context): SQLiteOpenHelper(context, DAT
 
 
     fun updateChore(chore: Chore): Int {
-        var db: SQLiteDatabase = writableDatabase
-        var values = ContentValues()
+        val db: SQLiteDatabase = writableDatabase
+        val values = ContentValues()
 
         //Putting a key-value into the Value Object
         values.put(KEY_CHORE_NAME, chore.choreName)
@@ -101,22 +113,22 @@ class ChoresDatabaseHandler(val context: Context): SQLiteOpenHelper(context, DAT
         values.put(KEY_CHORE_ASSIGNED_TIME, System.currentTimeMillis())
 
         //Update a row
-        return db.update(TABLE_NAME, values, KEY_ID + "=?", arrayOf(chore.id.toString()))
+        return db.update(TABLE_NAME, values, "$KEY_ID=?", arrayOf(chore.id.toString()))
 
     }
 
 
-    fun deleteChore(chore: Chore) {
+    fun deleteChore(id: Int) {
 
-        var db: SQLiteDatabase = writableDatabase
+        val db: SQLiteDatabase = writableDatabase
 
-        db.delete(TABLE_NAME, KEY_ID + "=?", arrayOf(chore.id.toString()))
+        db.delete(TABLE_NAME, "$KEY_ID=?", arrayOf(id.toString()))
         db.close()
     }
     fun getChoreCount(): Int {
-        var db: SQLiteDatabase = readableDatabase
-        var countQuery = "SELECT * FROM $TABLE_NAME"
-        var cursor = db.rawQuery(countQuery, null)
+        val db: SQLiteDatabase = readableDatabase
+        val countQuery = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(countQuery, null)
 
         return cursor.count
 
